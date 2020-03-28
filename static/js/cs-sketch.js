d5fda7a4bd1e750e7658296a830d14850df4e626
david-kishi <dansdk.blue@gmail.com>
2020-03-28 00:42:31 -0700

    Implemented Gold's Pore Sort Algorithm

--- static/js/cs-sketch.js
+++ static/js/cs-sketch.js
@@ -2,138 +2,176 @@
 
 // Make global g_canvas JS 'object': a key-value 'dictionary'.
 var g_canvas = {
-    cell_size: 10,
-    wid: 41,
-    hgt: 41
+    cell_size: 20,
+    wid: 40,
+    hgt: 40
 }; // JS Global var, w canvas size info.
 
 var g_frame_cnt = 0; // Setup a P5 display-frame counter, to do anim
-var g_frame_mod = 1; // Update ever 'mod' frames.
+var g_frame_mod = 20; // Update ever 'mod' frames.
 var g_stop = 0; // Go by default.
 
-// 3-D Rule Index
-var rule_index = [
-    [
-        // 0,0,0 & 0,0,1
-        [false, true],
-        // 0,1,0 & 0,1,1
-        [true, false]
-    ],
-    [
-        // 1,0,0 & 1,0,1
-        [true, false],
-        // 1,1,0 & 1,1,1
-        [false, true]
-    ]
-]
+// Array of 24 Sample Inputs
+var inputs = [
+    ["0", "5", "A", "6", "2", "7", "B", "2", "B", "6", "0", "3"],
+    ["0", "6", "5", "6", "6", "7", "1", "0", "4", "0", "B", "A"],
+    ["0", "6", "8", "4", "B", "8", "9", "3", "5", "7", "5", "4"],
+    ["0", "7", "9", "A", "2", "1", "8", "3", "4", "B", "6", "5"],
+    ["0", "9", "4", "8", "7", "8", "6", "2", "2", "6", "1", "6"],
+    ["1", "A", "B", "3", "4", "7", "9", "0", "5", "2", "8", "6"],
+    ["2", "8", "6", "1", "0", "3", "4", "2", "7", "8", "5", "9"],
+    ["3", "0", "5", "3", "0", "4", "7", "8", "6", "A", "2", "1"],
+    ["3", "2", "8", "4", "7", "6", "5", "1", "0", "B", "A", "9"],
+    ["3", "4", "2", "7", "5", "6", "1", "8", "9", "0", "B", "A"],
+    ["4", "1", "B", "3", "8", "2", "6", "2", "1", "9", "8", "5"],
+    ["4", "6", "3", "7", "9", "0", "1", "5", "B", "8", "A", "2"],
+    ["5", "3", "5", "1", "A", "3", "3", "A", "9", "9", "B", "B"],
+    ["5", "9", "3", "4", "7", "9", "0", "8", "8", "A", "1", "5"],
+    ["5", "9", "A", "2", "2", "A", "4", "4", "A", "3", "9", "4"],
+    ["7", "1", "9", "2", "0", "6", "8", "B", "3", "4", "5", "A"],
+    ["7", "2", "B", "3", "A", "5", "4", "1", "6", "9", "8", "0"],
+    ["8", "1", "A", "3", "9", "2", "0", "1", "0", "A", "9", "1"],
+    ["8", "9", "4", "0", "A", "5", "2", "B", "1", "6", "3", "7"],
+    ["A", "6", "9", "3", "5", "4", "2", "B", "7", "0", "1", "8"],
+    ["A", "9", "4", "2", "5", "B", "1", "6", "8", "7", "3", "0"],
+    ["A", "A", "0", "2", "3", "B", "7", "2", "3", "5", "6", "4"],
+    ["B", "4", "0", "1", "6", "3", "8", "A", "2", "9", "7", "5"],
+    ["B", "5", "8", "6", "1", "7", "9", "2", "A", "4", "0", "3"]
+];
+
+// Select a random sample input
+var running_input = inputs[Math.floor(Math.random() * 24)];
+var converted_input = convertHexDec(running_input, "hex2dec");
+
+// TODO Merge Sort Object
+var merge_Obj = {
+    finish: false
+}
+
+// TODO Quick Sort Object
+var quick_Obj = {
+    finish: false
+}
+
+// Gold's Pore Object
+var gold_Obj = {
+    arr: converted_input,
+    finish: false,
+    s_pos: 0,
+    generation: 1
+}
+
+// Convert Function
+// Variables - source, hex2dec or dec2hex
+function convertHexDec(arr, type) {
+    let temp = [...arr];
+
+    switch (type) {
+        case "hex2dec": // Convert hex to dec
+            for (let i = 0; i < arr.length; i++) {
+                temp[i] = parseInt(arr[i], 16);
+            }
+            break;
+
+        case "dec2hex": // Convert dec to hex
+            for (let i = 0; i < arr.length; i++) {
+                temp[i] = arr[i].toString(16).toUpperCase();
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    // return array of chars
+    return temp;
+}
 
 function setup() // P5 Setup Fcn
 {
+    // Create grid
     let sz = g_canvas.cell_size;
     let width = sz * g_canvas.wid; // Our 'canvas' uses cells of given size, not 1x1 pixels.
     let height = sz * g_canvas.hgt;
     createCanvas(width, height).parent("demoCanvas"); // Make a P5 canvas.
     background(0);
-    draw_grid(10, 50, 'black');
+    draw_grid(20, 50, 'white');
 
-    // Initiate generation 0
-    fill(765);
-    stroke(765);
-    rect((int(g_canvas.wid / 2) * g_canvas.cell_size) + 0.75, 0.75, g_canvas.cell_size - 1.5, g_canvas.cell_size - 1.5);
+    // Set font and font size
+    textFont('Courier');
+    textSize(15);
+    textAlign(CENTER, CENTER);
+    fill(255, 255, 255);
 
+    // Add name of algorithm above each column
+    text("Merge Sort", 120, 10);
+    text("Quick Sort", 400, 10);
+    text("Gold's Pore Sort", 680, 10);
+
+    // Populate first row with randomly selected sample input
+    for (let i = 0; i < 12; i++) {
+        // Merge Sort
+        fill(converted_input[i] * 100, converted_input[i] * 10, converted_input[i] * 10);
+        square(20 * i, 20, 20);
+        fill(255, 255, 255);
+        text(running_input[i], (20 * i) + 10, 30);
+
+        // Quick Sort
+        fill(converted_input[i], converted_input[i] * 25, converted_input[i] * 15);
+        square((20 * i) + 280, 20, 20);
+        fill(255, 255, 255);
+        text(running_input[i], (20 * i) + 290, 30);
+
+        // Gold Pore Sort
+        fill(converted_input[i] * 10, converted_input[i] * 10, converted_input[i] * 100);
+        square((20 * i) + 560, 20, 20);
+        fill(255, 255, 255);
+        text(running_input[i], (20 * i) + 570, 30);
+    }
 }
 
-var g_bot = {
-    x: 0,
-    y: 1,
-};
+function gold_stepper(gold_Obj) {
+    if (!gold_Obj.finish) {
+        gold_Obj.generation++;
+        var swap = false;
 
-var g_box = {
-    t: 1,
-    hgt: g_canvas.hgt,
-    l: 1,
-    wid: g_canvas.wid
-};
+        // Traverse through array and run swaps
+        for (let i = gold_Obj.s_pos; i < 12 - gold_Obj.s_pos; i++) {
+            if (gold_Obj.arr[i + 1] < gold_Obj.arr[i]) {
+                let b = gold_Obj.arr[i + 1];
+                gold_Obj.arr[i + 1] = gold_Obj.arr[i];
+                gold_Obj.arr[i] = b;
+                swap = true;
+            }
+            i++;
+        }
 
-function move_bot() {
-    let dx = 1;
-
-    let x = (dx + g_bot.x + g_box.wid) % g_box.wid; // Move-x.  Ensure positive b4 mod.
-    let y = g_bot.y;
-
-    // if bot reaches end of row, increment y to move bot to next row
-    if (x == 0) {
-        y += 1;
-        if (y > g_box.hgt) { // if y is greater than box height, end
-            g_stop = !g_stop;
+        // Swap starting position
+        if (gold_Obj.s_pos == 1) {
+            gold_Obj.s_pos = 0;
         } else {
-            g_bot.y = y;
+            gold_Obj.s_pos = 1;
+        }
+
+        // Check finish flag
+        if (!swap) {
+            gold_Obj.finish = true;
+        }
+
+        // Populate row
+        let temp = convertHexDec(gold_Obj.arr, "dec2hex");
+        for (let i = 0; i < 12; i++) {
+            fill(gold_Obj.arr[i] * 10, gold_Obj.arr[i] * 10, gold_Obj.arr[i] * 100);
+            square((20 * i) + 560, (20 * gold_Obj.generation), 20);
+            fill(255, 255, 255);
+            text(temp[i], (20 * i) + 570, (20 * gold_Obj.generation) + 10);
         }
     }
-
-    g_bot.x = x; // Update bot x.
-
-}
-
-function draw_bot() // Convert bot pox to grid pos & draw bot.
-{
-    let sz = g_canvas.cell_size;
-    let sz2 = sz / 2;
-    let x = 0.75 + g_bot.x * sz; // Set x one pixel inside the sz-by-sz cell.
-    let y = 0.75 + g_bot.y * sz;
-    let big = sz - 1.5; // Stay inside cell walls.
-
-    // family of cells from prev gen; 0=white / 1=black
-    let left = me = right = 0;
-    let pix = 765; // white
-
-    // Peek into family of cells in previous generation
-    // Left
-    if (g_bot.x == 0) {
-        left = 0;
-    } else {
-        let lColors = get(x + sz2 - sz - 0.75, y + sz2 - sz - 0.75);
-        pix = lColors[0] + lColors[1] + lColors[2];
-        if (pix == 765) {
-            left = 1;
-        }
-    }
-
-    // Me
-    let mColors = get(x + sz2 - 0.75, y + sz2 - sz - 0.75);
-    pix = mColors[0] + mColors[1] + mColors[2];
-    if (pix == 765) {
-        me = 1;
-    }
-
-    // Right
-    if (g_bot.x == g_box.wid - 1) {
-        right = 0;
-    } else {
-        let rColors = get(x + sz2 + sz - 0.75, y + sz2 - sz - 0.75);
-        pix = rColors[0] + rColors[1] + rColors[2];
-        if (pix == 765) {
-            right = 1;
-        }
-    }
-
-    // Determine stroke and fill based on rule_index
-    if (rule_index[left][me][right]) {
-        fill('white');
-        stroke('white');
-    } else {
-        fill(0);
-        stroke(0);
-    }
-
-    // Paint the cell.
-    rect(x, y, big, big);
 }
 
 function draw_update() // Update our display.
 {
-    draw_bot();
-    move_bot();
+    gold_stepper(gold_Obj);
 }
 
 function draw() // P5 Frame Re-draw Fcn, Called for Every Frame.
